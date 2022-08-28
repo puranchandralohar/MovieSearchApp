081393f9332356663a5a233a9c9f98593c67c7b2
puranchandralohar <clpuran95@gmail.com>
2022-08-28 19:33:31 +0530

    Landing Page ready with fetching data

--- app.js
+++ app.js
@@ -3,28 +3,30 @@
 let form = document.querySelector(".search");
 let searchbox = document.querySelector(".searchbox");
 
-let main =document.querySelector(".main-content")
-
-// console.log(form,searchbox)
 
 form.addEventListener("submit", function (name) {
+  
   fetch(`https://api.tvmaze.com/search/shows?q=${searchbox.value}`)
     .then((responce) => responce.json())
     .then((data) => {
 
-      let result = `<div class="content">
-      <div class="poster">
-          <img src="" alt="">
-      </div>
-      <div class="show-title">
-          <h3>Name : Thrones 360</h3>
-          <h3>Start Date : 2011-6-17</h3>
-          <h5>Language: English</h5>
-          <button class="btn">Show Details</button>
-      </div>
-  </div>`;
-      main.innerHTML = result;
-      console.log(data);
-    })
-    .catch((err) => alert("invalid city name"));
+      for(let i=0 ; i < data.length;i++){
+        
+          let shows =data[i]["show"];
+
+              let result = `<div class="content">
+              <div class="poster">
+                  <img src="${shows.image.medium}" alt="">
+              </div>
+              <div class="show-title">
+                  <h3>Name :${shows.name}</h3>
+                  <h3>Start Date : ${shows.premiered}</h3>
+                  <h5>Language: ${shows.language}</h5>
+                  <button class="btn">Show Details</button>
+              </div>
+              </div>`;
+              let main =document.querySelector(".main-content")
+              main.innerHTML += result;
+      }
+    }).catch((err)=>alert('Not Found Any Show'))
 });
